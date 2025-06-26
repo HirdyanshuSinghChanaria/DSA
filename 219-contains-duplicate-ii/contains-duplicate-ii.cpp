cff18a709d0783c620504b73f6b271858377f796
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2025-06-26 14:19:25 +0530

    Time: 1586 ms (6.96%) | Memory: 73.9 MB (96.71%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;(j<n&&j<=i+k);j++){
-                if((nums[i]==nums[j])&& (abs(i-j)<=k)) return true;
+                if((nums[i]==nums[j])) return true;
             }
         }
         return false;
