ba48cacff46a9df84bdceb927ea90df4140310e7
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2025-06-30 21:31:27 +0530

    Time: 22 ms (55.12%) | Memory: 43.8 MB (59.83%) - LeetSync

--- 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.cpp
@@ -3,16 +3,11 @@ public:
     int findLHS(vector<int>& nums) {
         int maxi = 0;
         unordered_map<int, int> mp;
-
         for (int num : nums) {
             mp[num]++;
         }
-
         for (auto it: mp) {
-            if (mp.count(it.first + 1)) {
-                int temp = it.second + mp[it.first + 1];
-                maxi = max(maxi, temp);
-            }
+            if (mp.count(it.first + 1))  maxi = max(maxi,it.second + mp[it.first + 1]);   
         }
         return maxi;
     }
