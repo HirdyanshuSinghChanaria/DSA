e9460efb1176e812ed2e14fccf39b107c150b622
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2025-09-11 20:20:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (41.89%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int dis;
+        auto it=lower_bound(nums.begin(),nums.end(),target);
+        if(it!=nums.begin() && (*it==target)) dis=distance(nums.begin(),it);
+        else dis=distance(nums.begin(),it);
+        return dis;
+    }
+};
