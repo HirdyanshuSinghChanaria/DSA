e62f67c23de9ef51cf8b295d58d074b9e4401c17
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2025-10-07 11:03:09 +0530

    Time: 23 ms (5.69%) | Memory: 48.7 MB (9.88%) - LeetSync

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         int n=g.size();
         priority_queue<int,vector<int>,greater<int>>gz,sz;
         for(auto it:g) gz.push(it);
