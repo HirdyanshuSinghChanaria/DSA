a6195a27b21b987c4885ad303f26e9829e936621
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2025-10-14 20:35:03 +0530

    Time: 3 ms (99.11%) | Memory: 40.7 MB (23.89%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.cpp
@@ -3,6 +3,7 @@ public:
     bool hasIncreasingSubarrays(vector<int>& nums, int k) {
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
+        cout.tie(nullptr);
         int i=0,j,n=nums.size();
         if(n==2 || k==1) return true;
         while(i+2*k<=n){
