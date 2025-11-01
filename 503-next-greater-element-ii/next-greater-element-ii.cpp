6dcfe0dd5972898bed451215e963c0ad4133082c
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2025-11-02 01:10:21 +0530

    Time: 1349 ms (5.04%) | Memory: 392.5 MB (5.26%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -5,12 +5,15 @@ public:
         stack<int>st1, st2;
         vector<int>ans;
         for(int i = n-1; i>=0; i--)st1.push(arr[i]);
-        deque<int>hash;
+        vector<int>hash;
         while(!st1.empty()){
             int x = st1.top();
             cout<<(int)st1.size()<<endl;
             st1.pop();
-            hash.push_back(x);
+            if(hash.empty() or hash.back()< x){
+                hash.push_back(x);
+            }
+            else hash.push_back(hash.back());
             while(!st1.empty() and st1.top() <= x){
                 int xx = st1.top();
                 st2.push(xx);
@@ -29,14 +32,12 @@ public:
                     st1.push(st2.top());
                     st2.pop();
                 }
-                for(auto it: hash){
-                    if(it > x){
-                        ans.push_back(it);
-                        check = 1;
-                        break;
-                    }
+                
+                auto it = upper_bound(hash.begin(), hash.end(), x);
+                if(it == hash.end()){
+                    ans.push_back(-1);
                 }
-                if(!check)ans.push_back(-1);
+                else ans.push_back(*it);
 
             }
 
