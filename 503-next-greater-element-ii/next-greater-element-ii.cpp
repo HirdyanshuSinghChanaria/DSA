eb7de34945c1e06980a16546fcc640db27a742d2
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2025-11-04 00:53:06 +0530

    Time: 6 ms (42.65%) | Memory: 28.2 MB (89.97%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,45 +1,16 @@
 class Solution {
 public:
-    vector<int> nextGreaterElements(vector<int>& arr) {
-        int n = (int)arr.size();
-        stack<int>st1, st2;
-        vector<int>ans;
-        for(int i = n-1; i>=0; i--)st1.push(arr[i]);
-        vector<int>hash;
-        while(!st1.empty()){
-            int x = st1.top();
-            st1.pop();
-            if(hash.empty() or hash.back()< x){
-                hash.push_back(x);
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int>st;
+        int n=nums.size();
+        vector<int>ans(n,-1);
+        for(int i=0;i<2*n;i++){
+            int index=i%n;
+            while(!st.empty()&& nums[index]>nums[st.top()]){
+                ans[st.top()]=nums[index];
+                st.pop();
             }
-            else hash.push_back(hash.back());
-            while(!st1.empty() and st1.top() <= x){
-                int xx = st1.top();
-                st2.push(xx);
-                st1.pop();
-            }
-            if(!st1.empty() and st1.top() > x){
-                ans.push_back(st1.top());
-                while(!st2.empty()){
-                    st1.push(st2.top());
-                    st2.pop();
-                }
-            }
-            else{
-                int check =0;
-                while(!st2.empty()){
-                    st1.push(st2.top());
-                    st2.pop();
-                }
-                
-                auto it = upper_bound(hash.begin(), hash.end(), x);
-                if(it == hash.end()){
-                    ans.push_back(-1);
-                }
-                else ans.push_back(*it);
-
-            }
-
+            if(i<n)st.push(index);
         }
         return ans;
     }
