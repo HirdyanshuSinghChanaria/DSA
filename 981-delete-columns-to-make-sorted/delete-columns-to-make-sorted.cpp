97d59bc8c6b1b9b4aa8bd46911704418864bf717
Hirdyanshu <ranaanita097@gmail.com>
2025-12-23 12:06:27 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (48.94%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -4,12 +4,9 @@ public:
         int size=strs[0].length(),n=strs.size();
         int count=0;
         for(int i=0;i<size;i++){
-            vector<char>a(n);
-            int j;
-            for(j=1;j<n;j++){
+            for(int j=1;j<n;j++){
                 if(strs[j][i]<strs[j-1][i]){count++;break;}
             }
-            // if(j!=n) count++;
         }
         return count;
     }
