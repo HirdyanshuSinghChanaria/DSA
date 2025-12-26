f866aec3d41a26ba9bf2bc7d3d3846182130f73b
Hirdyanshu <ranaanita097@gmail.com>
2025-12-26 18:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.89%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -1,18 +1,20 @@
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 class Solution {
 public:
-    int bestClosingTime(string customers) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
+    int bestClosingTime(const string& customers) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int max_score=0;
         int cur_score=0;
         int best_hour=0;
-        int n = customers.size();
-        for(int i=0;i<n;i++) {
-            if(customers[i]=='Y') cur_score++;
-            else cur_score--;
-            if(cur_score>max_score) {
-                max_score = cur_score;
-                best_hour = i + 1;
+        const char* str=customers.c_str();
+        const int n=customers.size();
+        for(int i=0;i<n;++i){
+            cur_score+=(str[i]=='Y')*2-1;
+            if(cur_score>max_score){
+                max_score=cur_score;
+                best_hour=i + 1;
             }
         }
         return best_hour;
