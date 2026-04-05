a790f630f2dbb40ec16acc56828fc916f0f2c6c2
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2026-04-06 01:02:01 +0530

    Time: 8 ms (10.20%) | Memory: 10.3 MB (47.26%) - LeetSync

--- 657-robot-return-to-origin/robot-return-to-origin.cpp
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
         if(moves.length()%2!=0) return false;
         unordered_map<char,int>mp;
         for(int i=0;i<moves.length();i++) mp[moves[i]]++;
