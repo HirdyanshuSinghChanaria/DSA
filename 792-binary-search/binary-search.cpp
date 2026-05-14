42db3e133db25021bb5509fa1834b65f228b1f13
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2026-05-14 22:40:06 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (7.08%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         auto it=lower_bound(nums.begin(),nums.end(),target);
-        if(it!=nums.end() && *it==target) return distance(nums.begin(),it);
-        return -1;
+        if(it==nums.end() || *it!=target) return -1;
+        return distance(nums.begin(),it);
     }
 };
