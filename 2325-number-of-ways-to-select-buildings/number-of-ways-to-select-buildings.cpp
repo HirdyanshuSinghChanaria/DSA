ee1cf64eb4a642b86e22a011b3e0eb3b3474b99b
Hirdyanshu <112776698+HirdyanshuSinghChanaria@users.noreply.github.com>
2026-07-22 16:39:51 +0530

    Time: 16 ms (88.24%) | Memory: 27.7 MB (92.35%) - LeetSync

--- 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.cpp
+++ 2325-number-of-ways-to-select-buildings/number-of-ways-to-select-buildings.cpp
@@ -3,7 +3,7 @@ public:
     long long numberOfWays(string s) {
         int n=s.length();
         long long ones=0,zeros=0;
-        for(int i=0;i<n;i++){(s[i]=='1' ? ones : zeros)++;}
+        for(int i=0;i<n;i++){(s[i]=='1' ? ones++ : zeros++);}
         long long leftzero=0,leftone=0;
         long long ans=0;
         for(int i=0;i<n;i++){
